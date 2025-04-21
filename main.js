3ca1d895d3ab13987d2152031946efa349acb612
Yasir <yasir.hussain@vertexitsol.com>
2025-04-21 20:32:43 +0500

    add gitignore file

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 const { app, BrowserWindow } = require('electron');
 
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV != 'production' ? true : false;
+const isMac = process.platform == 'darwin' ? true : false;
+console.log(process.platform)
+
+let win;
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + '/profile-pic.png',
+    resizable: isDev ? true : false,
   });
 
   win.loadFile('index.html');
+   // Open DevTools
+  //  win.webContents.openDevTools();
+
+
+
 }
 
-app.whenReady().then(createWindow);
+app.on ('ready' , createWindow);
+
+app.on('window-all-closed' ,() => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
