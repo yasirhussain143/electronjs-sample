41a1d9e931c18036349dfe942a78662d40c8edb9
Yasir <yasir.hussain@vertexitsol.com>
2025-04-21 22:44:26 +0500

    adding menu and globel shortkeys

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow ,Menu, globalShortcut} = require('electron');
 
 process.env.NODE_ENV = 'development';
 
@@ -13,6 +13,7 @@ function createWindow() {
     height: 600,
     icon: __dirname + '/profile-pic.png',
     resizable: isDev ? true : false,
+    backgroundColor: '#2f3156',
   });
 
   win.loadFile('index.html');
@@ -23,7 +24,34 @@ function createWindow() {
 
 }
 
-app.on ('ready' , createWindow);
+app.on ('ready' , () => {
+  createWindow()
+
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+  
+  globalShortcut.register('CommandOrControl+R', () => win.reload())
+  globalShortcut.register('CommandOrControl+Shift+I', () => win.webContents.toggleDevTools())
+
+
+    win.on('close', () => win ='null' )
+  })
+
+const menu = [
+  ...(isMac ? [{role: 'appMenu'}]  :[]),
+    {
+      label: 'File',
+      submenu:[
+        {
+          label: 'Quit',
+          accelerator:  isMac ? 'CmdOrCtrl+Q' : 'Ctrl+Q',
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    }
+]
 
 app.on('window-all-closed' ,() => {
   if (!isMac) {
